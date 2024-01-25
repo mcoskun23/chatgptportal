3451ba7a9daff9f32f2335040f938be28040311b
mcoskun23 <mustafacoskun6@outlook.com>
2024-01-25 22:40:57 +0300

    wizard

--- webapp/controller/Create.controller.ts
+++ webapp/controller/Create.controller.ts
@@ -1,4 +1,5 @@
 import BaseController from "./BaseController";
+import { createModel } from "../model/models";
 
 /**
  * @namespace com.ntt.chatgptportal.controller
@@ -7,6 +8,18 @@ export default class Create extends BaseController {
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
+        this.getRouter()?.getRoute("create")?.attachPatternMatched(this._onMatched, this);
+    }
+
+    private _onMatched() {
+        this.setModel(createModel(), "model");
+    }
+
+    private onPressAddParam() {
+
+    }
+
+    private onPressDeleteParam() {
 
     }
 }
